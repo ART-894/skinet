d51dfa62dfc59cca0a3ca5ed87996a76995e4c73
Abhi T <abhinavt.8794@gmail.com>
2022-08-09 17:51:16 +0530

    Section 4

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,8 +1,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Dtos;
+using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
+using Core.Specifications;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -13,37 +16,66 @@ namespace API.Controllers
   [Route("api/[controller]")]
   public class ProductsController : ControllerBase
   {
-    private readonly IProductRepository _repo;
+    private readonly IGenericRepository<Product> _productRepo;
+    private readonly IGenericRepository<ProductBrand> _productBrandRepo;
+    private readonly IGenericRepository<ProductType> _productTypeRepo;
+    private readonly IMapper _mapper;
 
-    public ProductsController(IProductRepository repo)
+    public ProductsController(IGenericRepository<Product> productRepo, IGenericRepository<ProductType> productTypeRepo,
+    IGenericRepository<ProductBrand> productBrandRepo, IMapper mapper)
     {
-      _repo = repo;
+      _mapper = mapper;
+      _productTypeRepo = productTypeRepo;
+      _productBrandRepo = productBrandRepo;
+      _productRepo = productRepo;
+
     }
 
     [HttpGet]
-    public async Task<ActionResult<List<Product>>> GetProducts()
+    public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts()
     {
-      var products = await _repo.GetProductsAsync();
+      var spec = new ProductWithTypesAndBrandsSpecification();
 
-      return Ok(products);
+      var products = await _productRepo.ListAsync(spec);
+
+      return Ok(_mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products));
     }
 
     [HttpGet("{id}")]
-    public async Task<ActionResult<Product>> GetProduct(int id)
+    /*
+    * On passing this id we create new instance of ProductWithTypesAndBrandsSpecification
+    * We hit the parameterized constructor of the above class
+    * We also create instance of BaseSpecification because ProductWithTypesAndBrandsSpecification 
+      is inherited from BaseSpecification.
+    * Next it hits the GetEntityWithSpec with spec in constructor in GenericRepository
+    * public async Task<T> GetEntityWithSpec(ISpecification<T> spec) which in turn will hit the
+    * private IQueryable<T> ApplySpecification(ISpecification<T> spec) 
+    * where it hits the SpecificationEvaluator<T>.GetQuery(_context.Set<T>().AsQueryable(), spec);
+    * _context.Set is the product db set 
+    * and also the spec which is specification from ProductWithTypesAndBrandsSpecification
+    * here we put the criteria in where clause like productID
+    * then it hits aggregate of include methods like productBrand and Type
+    * this returns a query, this IQueryable is then passed to the dbContext and in Db
+    * this is finally returned in the method below.
+    */
+    public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
     {
-      return await _repo.GetProductByIdAsync(id);
+      var spec = new ProductWithTypesAndBrandsSpecification(id);
+      var product = await _productRepo.GetEntityWithSpec(spec);
+
+      return _mapper.Map<Product, ProductToReturnDto>(product);
     }
 
     [HttpGet("brands")]
     public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
     {
-      return Ok(await _repo.GetProductBrandsAsync());
+      return Ok(await _productBrandRepo.ListAllAsync());
     }
 
     [HttpGet("types")]
     public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
     {
-      return Ok(await _repo.GetProductTypesAsync());
+      return Ok(await _productTypeRepo.ListAllAsync());
     }
 
   }
